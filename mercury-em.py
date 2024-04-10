4855d857f223984c365955225e90220936a7db0b
h8axstella <166030410+h8axstella@users.noreply.github.com>
2024-04-10 11:00:17 +0300

    Update mercury-em.py

--- mercury-em.py
+++ mercury-em.py
@@ -9,46 +9,66 @@
 #
 
 import argparse
-import sys
 import socket
 import json
 import mercury.mercury206 as mercury206
 import mercury.mercury236 as mercury236
 
-def parse_cmd_line_args():
-    parser = argparse.ArgumentParser(description="Mercury energy meter data receiver")
 
-    parser.add_argument("--proto", choices=["m206", "m236"],  nargs='?', default="m206", help='Mercury protocol (M206/M236)')
-    parser.add_argument('--serial', type=int,  nargs='?', default=0, help='Device serial number', required=True)
+def parse_cmd_line_args():
+    parser = argparse.ArgumentParser(description="Mercury energy meter data receiver",
+                                     formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument("--proto", choices=["m206", "m236"], nargs='?', default="m206",
+                        help='Mercury protocol (M206/M236)')
+    parser.add_argument('--serial', type=int, nargs='?', default=0, help='Device serial number', required=True)
 
     parser.add_argument('--host', type=str, nargs='?', default=0, help='RS485-TCP/IP Convertor IP.')
     parser.add_argument('--port', type=int, nargs='?', default="50", help='RS485-TCP/IP Convertor (default: 50)')
 
-    parser.add_argument('--user', choices=["user", "admin"], default="user", nargs='?', help='Device user (for m236 proto)')
+    parser.add_argument('--user', choices=["user", "admin"], default="user", nargs='?',
+                        help='Device user (for m236 proto)')
     parser.add_argument('--pass', dest="passwd", type=str, nargs='?', help='Device password (for m236 proto)')
 
-    parser.add_argument('--format',  choices=["text", "json", "human"],  nargs='?', default="json", help='Output format')
+    parser.add_argument('--format', choices=["text", "json", "human"], nargs='?', default="json", help='Output format')
+
+    parser.add_argument('--array-number',
+                        choices=[0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x09, 0x0A, 0x0B, 0x0C, 0x0D],
+                        type=int,
+                        nargs='?',
+                        default=0x00,
+                        help=('Номер массива\n'
+                              '  0  - От сброса (по умолчанию)\n'
+                              '  1  - За текущий год\n'
+                              '  2  - За предыдущий год\n'
+                              '  3  - За месяц\n'
+                              '  4  - За текущие сутки\n'
+                              '  5  - За предыдущие сутки\n'
+                              '  6  - Пофазные значения учтенной активной энергии прямого направления\n'
+                              '  9  - На начало текущего года\n'
+                              '  10 - На начало предыдущего года\n'
+                              '  11 - На начало месяца\n'
+                              '  12 - На начало текущих суток\n'
+                              '  13 - На начало предыдущих суток\n'
+                              ))
 
     return parser.parse_args()
 
 
-
-def print_output_text(arr, prefix = ""):
+def print_output_text(arr, prefix=""):
     for key, value in arr.items():
         if isinstance(value, dict):
-            print_output_text( value, prefix + "." + key )
+            print_output_text(value, prefix + "." + key)
         else:
             print(f"{prefix}.{key}={value}")
 
 
-def print_output(arr, format):
-    if format == "text":
+def print_output(arr, output_format):
+    if output_format == "text":
         print_output_text(arr)
 
-    elif format == "json":
-        print (json.dumps(arr))
-
-
+    elif output_format == "json":
+        print(json.dumps(arr))
 
 
 if __name__ == "__main__":
@@ -69,8 +89,6 @@ if __name__ == "__main__":
 
             result['energy'] = mercury206.read_energy(sock, args.serial)
         except TimeoutError:
-            result['error'] = "Timeout while read data from socket"
-        except socket.timeout:
             result['error'] = "Timeout while read data from socket"
         except ValueError:
             result['error'] = "Wrong data"
@@ -97,14 +115,16 @@ if __name__ == "__main__":
 
         args.user = 0x02 if args.user == "admin" else 0x01
         if not args.passwd:
-            args.passwd = "222222" if args.user==2 else "111111"
+            args.passwd = "222222" if args.user == 2 else "111111"
 
         try:
             mercury236.check_connect(sock, args.serial)
             mercury236.open_channel(sock, args.serial, args.user, args.passwd)
 
-            result['energy_phases_AR'] = mercury236.read_energy_sum_act_react(sock, args.serial)
-            result['energy_tarif_AR'] = mercury236.read_energy_tarif_act_react(sock, args.serial)
+            result[f'energy_phases_{args.array_number}'] = mercury236.read_energy_sum_act_react(sock, args.serial,
+                                                                                                param=args.array_number)
+            result[f'energy_tarif_{args.array_number}'] = mercury236.read_energy_tarif_act_react(sock, args.serial,
+                                                                                                 param=args.array_number)
             result['energy_phases'] = mercury236.read_energy_sum_by_phases(sock, args.serial)
             result['energy_tarif'] = mercury236.read_energy_tarif_by_phases(sock, args.serial)
 
@@ -114,13 +134,10 @@ if __name__ == "__main__":
             mercury236.close_channel(sock, args.serial)
         except TimeoutError:
             result['error'] = "Timeout while read data from socket"
-        except socket.timeout:
-            result['error'] = "Timeout while read data from socket"
         except ValueError:
             result['error'] = "Wrong data"
 
         finally:
             sock.close()
-
 
     print_output(result, args.format)
