c9d57f7638d518b7b679dfe3af4b2b0cae09ed7a
h8axstella <166030410+h8axstella@users.noreply.github.com>
2024-04-10 11:13:51 +0300

    Update mercury236.py

--- mercury/mercury236.py
+++ mercury/mercury236.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from minimalmodbus import _calculate_crc_string as modbus_crc
-
 from struct import pack, unpack
 from typing import Union, Sequence
 from .utils import digitize
 
+def send_tcp_command(s, address_mercury, command, *params, **kwargs):
+    address = pack('!B', address_mercury)
+    params = bytes(params)
+    message = address + params
+    if kwargs.get('passwd'):
+        passwd = bytes([int(c) for c in kwargs.get('passwd')])
+        message += passwd
+    if kwargs.get('crc', True):
+        message += modbus_crc(message.decode('latin1')).encode('latin1')
+    s.sendall(message)
+    answer = read_data_from_socket(s)
+    if len(answer) > 1:
+        received_address, received_data = unpack_msg(answer)
+        if received_address == address_mercury:
+            return received_data
+    raise ValueError("Error while read data from socket")
 
 def read_vap(s, address_mercury, cmd=0x08):
     ''' Read Voltage (V), Amperage (A), Power (kW/h)
@@ -135,34 +150,22 @@ ADDRESS_FMT = '!B'  # unsigned char (integer 1 byte in network order)
 
 
 def read_data_from_socket(s):
-    data = ''
     buffer = b""
-
-    while not data:
+    while True:
         s.settimeout(1)
-        data = s.recv(1000)
-        if data:
-            buffer += data
-
+        try:
+            data = s.recv(1000)
+            if data:
+                buffer += data
+            else:
+                break  # Если данные не были получены, прерываем цикл
+        except socket.timeout:
+            break  # Прерываем цикл, если время ожидания истекло
+        except Exception as e:
+            print(f"Error while reading from socket: {e}")
+            break
     s.settimeout(None)
     return buffer
-
-
-def send_tcp_command(s, address_mercury, command, *params, **kwargs):
-    message = pack_msg(address_mercury, command, *params, passwd=kwargs.get('passwd', ''), crc=kwargs.get('crc', True))
-    s.sendall(message)
-
-    answer = read_data_from_socket(s)
-    # answer_lines = answer.split('\r\n')
-    # answer = ''.join(answer_lines)
-
-    if len(answer) > 1:
-        received_address, received_data = unpack_msg(answer)
-        if received_address == address_mercury:
-            return received_data
-
-    raise ValueError(f"Error while read data from socket")
-
 
 def pack_msg(address: Union[int, bytes], *args: Sequence[int], **kwargs) -> bytes:
     r"""Pack power meter address and args into string,
@@ -214,16 +217,6 @@ def pack_msg(address: Union[int, bytes], *args: Sequence[int], **kwargs) -> byte
 
 
 def unpack_msg(message: bytes):
-    r"""Unpack message string.
-    Assume the first 4 bytes carry power meter address
-    Return tuple with: integer power meter address and list of bytes
-    >>> unpack_msg(b'\x00\xA6\xB7\x20\x28')
-    (10925856, [40])
-    >>> unpack_msg(b'\x00\xA6\xB7\x20\x27\x00\x26\x56\x16\x00\x13\x70\x91\x00\x00\x00\x00\x00\x00\x00\x00\x47\x78')
-    (10925856, [39, 0, 38, 86, 22, 0, 19, 112, 145, 0, 0, 0, 0, 0, 0, 0, 0, 71, 120])
-    >>> unpack_msg(b'\x00\xA6\xB7\x20')
-    (10925856, [])
-    """
-    address = unpack(ADDRESS_FMT, message[:1])[0]
+    address = unpack('!B', message[:1])[0]
     data = list(message[1:])
     return address, data
